7d1b5f488523cdf404093a4c31cf7a041310b05d
Karol Mąkowski <Karol Mkowski>
2016-07-31 17:03:33 +0200

    midday commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,20 @@
-$('body').scrollspy({
-	target: '#home'
+$(document).ready(function(){	
+	// var $thumbHeight = $('#whatIDid .thumbnail:first').height();
+	// $('.thumbnail').height($thumbHeight);
+	// console.log('$thumbHeight', $thumbHeight);
+
+	$('body a').on('click', function(event){
+
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top}, 400, function(){
+				window.location.hash = hash;
+			});
+		};
+	});
 });
-$("#home a").on('click', function(event){
 
-	this.src='http://i.imgur.com/guRT5Db.png?1';
-
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash (#)
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    } // End if statement
-  };
-});
+  
+	
