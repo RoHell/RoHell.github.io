b77c2fffa69501f825be2bd9105cae08b8cde3b2
RoHell <karol-roch@wp.pl>
2016-07-31 03:01:53 +0200

    bootstrap thumbnails

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-$('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
-    menu: '.menu'
+$('body').scrollspy({
+	target: '#home'
+});
+$("#home a").on('click', function(event){
+
+	this.src='http://i.imgur.com/guRT5Db.png?1';
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash (#)
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    } // End if statement
+  };
 });
