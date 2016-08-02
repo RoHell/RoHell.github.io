9f090000c2f0d687784337d0d3e647aea844db6c
Karol Mąkowski <Karol Mkowski>
2016-08-03 01:23:02 +0200

    animation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
-$(document).ready(function(){	
-	// var $thumbHeight = $('#whatIDid .thumbnail:first').height();
-	// $('.thumbnail').height($thumbHeight);
-	// console.log('$thumbHeight', $thumbHeight);
-
+$(function(){
+	// === Smooth Scroll ===
 	$('body a').on('click', function(event){
-
 		if (this.hash !== "") {
 			event.preventDefault();
 			var hash = this.hash;
 			$('html, body').animate({
-				scrollTop: $(hash).offset().top}, 400, function(){
+			scrollTop: $(hash).offset().top},
+			400,
+			function(){
 				window.location.hash = hash;
 			});
 		};
 	});
+
+	// === Animate on Scroll ===
+	var $animation_elements = $('.slider-1');
+	var slideInLeft = 'slideInLeft animated';
+	var $window = $(window);
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
+
+	function check_if_in_view() {
+		var window_height = $window.height();
+		var window_top_position = $window.scrollTop();
+		var window_bottom_position = (window_top_position + window_height);
+
+		$.each($animation_elements, function() {
+			var $element = $(this);
+			var element_height = $element.outerHeight();
+			var element_top_position = $element.offset().top;
+			var element_bottom_position = (element_top_position + element_height);
+
+			//check to see if this current container is within viewport
+			if ((element_bottom_position >= window_top_position) &&
+				(element_top_position <= window_bottom_position)) {
+				$element.addClass(slideInLeft);
+			} 
+			// else {
+			// 	$element.removeClass(slideInLeft);
+			// }
+		});
+	}
 });
